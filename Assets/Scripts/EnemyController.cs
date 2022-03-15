f2a407749e979f7ca0f6deb1decdb5b24ce3ba38
八木暁史 <>
2022-03-16 00:13:13 +0900

    手順９　－敵キャラの移動方向と移動アニメの同期処理の実装－途中ー

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -14,13 +14,66 @@ public class EnemyController : MonoBehaviour
 
     private Vector3[] paths;// 移動する各地点を代入するための配列
 
+    private Animator anim;// Animator コンポーネントの取得用
+
+    private Vector3 currentPos;// 敵キャラの現在の位置情報
+
     void Start()
     {
+        // Animator コンポーネントを取得して anim 変数に代入
+        TryGetComponent(out anim);
+
         // 移動する地点を取得
         paths = pathData.pathTranArray.Select(x=>x.position).ToArray();
 
         // 各地点に向けて移動
         transform.DOPath(paths, 1000 / moveSpeed).SetEase(Ease.Linear);
     }
+
+    private void Update()
+    {
+        // 敵の進行方向を取得
+        ChangeAnimeDirection();
+    }
+
+    /// <summary>
+    /// 敵の進行方向を取得して、移動アニメと同期
+    /// </summary>
+
+    private void ChangeAnimeDirection()
+    {
+        if (transform.position.x < currentPos.x)
+        {
+            anim.SetFloat("Y", 0f);
+            anim.SetFloat("X", -1.0f);
+
+            Debug.Log("左方向");
+        }
+        else if(transform.position.y > currentPos.y)
+        {
+            anim.SetFloat("X", 0f);
+            anim.SetFloat("Y", 1.0f);
+
+            Debug.Log("上方向");
+        }
+        else if (transform.position.y < currentPos.y)
+        {
+            anim.SetFloat("X", 0f);
+            anim.SetFloat("Y", -1.0f);
+
+            Debug.Log("下方向");
+        }
+        else if (transform.position.x > currentPos.x)
+        {
+            anim.SetFloat("Y", 0f);
+            anim.SetFloat("X", 1.0f);
+
+            Debug.Log("右方向");
+        }
+
+        // 現在の位置情報を保持
+        currentPos = transform.position;
+    }
+
 }
 
