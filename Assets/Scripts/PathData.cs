f931eed393c4ec69399898089de4767e39e3d66b
八木暁史 <>
2022-03-15 21:51:34 +0900

    手順７　－敵キャラの作成と移動処理の実装－

---
+++ Assets/Scripts/PathData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathData : MonoBehaviour
+{
+    public Transform generateTran; // 敵の生成地点
+    public Transform[] pathTranArray; // 敵の移動経路を順番に登録する
+}
